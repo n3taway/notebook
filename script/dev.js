518c8242a7537befbd7af662cc47d48cbbb72f21
n3taway <n3taway@gmail.com>
2020-03-05 23:12:58 +0800

    docs: 添加 Github + Travis CI自动化部署Blog

--- script/dev.js
+++ script/dev.js
@@ -16,7 +16,7 @@ async function localService() {
         port: 8080, // Set the server port. Defaults to 8080.
         host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
         root: "./", // Set root directory that's being served. Defaults to cwd.
-        open: false, // When false, it won't load your browser by default.
+        open: true, // When false, it won't load your browser by default.
         file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
         logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
     };
